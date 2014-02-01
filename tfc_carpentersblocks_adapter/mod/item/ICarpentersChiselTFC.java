88d38a76ee3116df1b9c79317cfd08221d390712
Michael Gooch <mgooch@kotoro.com>
2014-02-01 04:20:29 -0500

    created classes for tfc versions of carpenters hammer & chisel

---
+++ tfc_carpentersblocks_adapter/mod/item/ICarpentersChiselTFC.java
@@ -0,0 +1,12 @@
+package tfc_carpentersblocks_adapter.mod.item;
+
+import carpentersblocks.item.ItemCarpentersChisel;
+
+public class ICarpentersChiselTFC extends ItemCarpentersChisel {
+
+	public ICarpentersChiselTFC(int itemID) {
+		super(itemID);
+		// TODO Auto-generated constructor stub
+	}
+
+}
